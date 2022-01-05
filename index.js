30bac369caed16914ea5302e321c1f5df7816360
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2022-01-06 00:45:56 +0900

    Added code for server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("index.js");
+import express from "express";
+
+const app = express();
+
+const getHome = (req, res) => {
+    res.send("hello");
+}
+
+app.get("/", getHome);
+
+app.listen(4000, () => {console.log("listen 4000 port")})
