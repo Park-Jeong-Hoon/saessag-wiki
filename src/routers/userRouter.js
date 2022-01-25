4f24565fbe6792c2fb19bbd83e9f56dc5778ff75
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2022-01-25 12:25:48 +0900

    Modified some mistakes

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { getEdit, getSee } from "../controllers/contentController";
-import { getChangePasswd, logout } from "../controllers/userController";
+import { getChangePasswd, getEdit, getSee, logout } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get("/:id", getSee);
 userRouter.get("/logout", logout);
 userRouter.get("/edit", getEdit);
 userRouter.get("/change-password", getChangePasswd);
+userRouter.get("/:id", getSee);
 
 export default userRouter;
