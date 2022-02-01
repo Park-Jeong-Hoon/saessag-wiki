bce597ff86c594ccc83a16c9bf29e3f12d1b04c4
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2022-02-01 18:18:45 +0900

    Modified route

--- src/routers/contentRouter.js
+++ src/routers/contentRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getDelete, getEdit, getSee, getUpload, postUpload } from "../controllers/contentController";
+import { getDelete, getEdit, getUpload, postUpload, see } from "../controllers/contentController";
 
 const contentRouter = express.Router();
 
-contentRouter.get("/:id(\\d+)", getSee);
+contentRouter.get("/:id([0-9a-f]{24})", see);
 contentRouter.route("/upload").get(getUpload).post(postUpload);
-contentRouter.get("/:id(\\d+)/edit", getEdit);
-contentRouter.get("/:id(\\d+)/delete", getDelete);
+contentRouter.get("/:id([0-9a-f]{24})/edit", getEdit);
+contentRouter.get("/:id([0-9a-f]{24})/delete", getDelete);
 
 export default contentRouter;
