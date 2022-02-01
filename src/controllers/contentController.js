121426ef499965500a53d585fbd7e392c35b3119
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2022-02-01 18:18:17 +0900

    Modified getHome, see

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -1,17 +1,23 @@
 import Content from "../models/Content";
 
-export const getHome = (req, res) => {
-    res.render("home", {pageTitle: "home"});
+export const getHome = async (req, res) => {
+    const contents = await Content.find({});
+    res.render("home", {pageTitle: "Home", contents});
 }
 
 export const getSearch = (req, res) => {
     const keyword = req.query.keyword;
     console.log(keyword);
-    res.render("search", { pageTitle: `searched ${keyword}`, keyword });
+    res.render("search", { pageTitle: `Searched ${keyword}`, keyword });
 }
 
-export const getSee = (req, res) => {
-    res.send("SeeContent");
+export const see = async (req, res) => {
+    const { id } = req.params;
+    const content = await Content.findById(id);
+    if (!content) {
+        res.status(404).render("404", { pageTitle: "해당 단어에 관한 내용이 없습니다." })
+    }
+    res.render("see-content", { pageTitle: "See-Content", content });
 }
 
 export const getUpload = (req, res) => {
