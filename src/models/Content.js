7a799ad340316b3701e2ed3e41b420cf9c192862
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2022-02-06 16:53:50 +0900

    Added owner at contentSchema

--- src/models/Content.js
+++ src/models/Content.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const contentSchema = new mongoose.Schema({
     word: { type: String },
-    explanation: { type: String }
+    explanation: { type: String },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
 const Content = mongoose.model("Content", contentSchema);
